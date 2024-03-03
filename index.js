ca9455bacf26cb2aa7bf454b7802570f47c00a49
NisalDeZoysa <90953837+NisalDeZoysa@users.noreply.github.com>
2024-03-03 13:09:31 +0530

    backend code with jswbtk

---
+++ index.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const cors = require("cors");
+
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+
+//connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(port, () => {
+      //port = process.env.PORT;
+      console.log("listning on port " + port + " connected to the DB");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+port = process.env.PORT;
