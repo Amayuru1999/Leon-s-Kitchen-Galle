5679b24c0d2edabe01db8d465c066d2461e479fe
NisalDeZoysa <90953837+NisalDeZoysa@users.noreply.github.com>
2024-03-05 18:09:50 +0530

    new changes-users view

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,14 +2,19 @@ import Chart from "../../components/chart/Chart";
 import FeaturedInfo from "../../components/featuredinfo/FeaturedInfo";
 import "./home.css";
 import { userData } from "../../dummyData";
-
 import React from "react";
+import WidgetSm from "../../components/widgetSm/WidgetSm.jsx";
+import WidgetLg from "../../components/widgetLg/WidgetLg.jsx";
 
 export default function Home() {
   return (
     <div className="home">
       <FeaturedInfo />
       <Chart data={userData} title="Sales Analytics" grid dataKey="sales" />
+      <div className="homeWidgets">
+        <WidgetSm />
+        <WidgetLg />
+      </div>
     </div>
   );
 }
