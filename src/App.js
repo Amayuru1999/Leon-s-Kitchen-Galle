1e420b0c18ff67e9dec200245ab671439b86e3e9
NisalDeZoysa <90953837+NisalDeZoysa@users.noreply.github.com>
2024-03-06 00:37:54 +0530

    Dashboard UI Done

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
 import NewUser from "./pages/newUser/NewUser";
 import ProductList from "./pages/productList/ProductList";
+import Product from "./pages/product/Product";
+import NewProduct from "./pages/newProduct/NewProduct";
 
 function App() {
   return (
@@ -21,6 +23,8 @@ function App() {
           <Route path="/user/:userId" element={<User />} />
           <Route path="/newUser" element={<NewUser />} />
           <Route path="/products" element={<ProductList />} />
+          <Route path="/product/:productId" element={<Product />} />
+          <Route path="/newproduct" element={<NewProduct />} />
         </Routes>
       </div>
     </Router>
