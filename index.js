938810e726b801dc61177d8d65685f719d72ca70
NisalDeZoysa <90953837+NisalDeZoysa@users.noreply.github.com>
2024-03-12 18:53:06 +0530

    comments on stripe

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
+//const stripeRoute = require("./routes/stripe");
 
 const app = express();
 
@@ -21,6 +22,7 @@ app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
+//app.use("/api/checkout", stripeRoute);
 
 //connect to db
 mongoose
