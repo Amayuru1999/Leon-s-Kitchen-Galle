be46e7866eb1c480f311f2ff1e82de076056bfa2
Amayuru1999 <amarasingheau@gmail.com>
2024-05-27 04:03:52 +0530

    Dashboard Page Mobile Responsived

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const mongoose=require('mongoose');
-mongoose.connect(process.env.MONGODB_URI,{useNewUrlParser: true,useUnifiedTopology:true,})
+mongoose.connect(process.env.MONGODB_URI)
 .then(()=> {
     console.log("DB Connected");
 })
